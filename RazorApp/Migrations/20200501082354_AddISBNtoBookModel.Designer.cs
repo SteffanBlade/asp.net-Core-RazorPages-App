a6d5a35153315e868fe71d2fac643809ccc153db
Mihai <mihaiionstefan@gmail.com>
2020-05-01 12:12:56 +0300

    Added CreateBook Page Added Post form and method

--- RazorApp/Migrations/20200501082354_AddISBNtoBookModel.Designer.cs
+++ RazorApp/Migrations/20200501082354_AddISBNtoBookModel.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorApp.Model;
 
 namespace RazorApp.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200501082354_AddISBNtoBookModel")]
+    partial class AddISBNtoBookModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorApp/Model/Book.cs
+++ RazorApp/Model/Book.cs
@@ -13,6 +13,7 @@ namespace RazorApp.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 
 }
